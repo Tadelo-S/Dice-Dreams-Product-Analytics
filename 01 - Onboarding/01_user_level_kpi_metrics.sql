96e82c088b551b24609d3bdc674ef9afec185384
Tadelo-S <tadelo658@gmail.com>
2026-03-28 18:34:16 +0300

    Add files via upload

--- 01 - Onboarding/01_user_level_kpi_metrics.sql
+++ 01 - Onboarding/01_user_level_kpi_metrics.sql
@@ -1,1 +1,19 @@
-
+SELECT
+  user_id,
+  app_version,
+  MAX(
+    CASE
+      WHEN event_type = 'tutorial_ended' AND completed = TRUE THEN 1
+      ELSE 0
+      END) AS is_complete,
+  MAX(
+    CASE
+      WHEN event_type = 'tutorial_ended' AND completed = TRUE THEN total_time
+      END)
+    / 60 AS completion_time_minutes,
+  MAX(CASE WHEN event_type = 'tutorial_error' THEN 1 ELSE 0 END) AS had_error,
+  MAX(CASE WHEN event_type = 'tutorial_step_skip' THEN 1 ELSE 0 END)
+    AS did_skip,
+  COUNT(CASE WHEN event_type = 'tutorial_error' THEN 1 END) AS total_errors
+FROM `ppltx-ba-course.final_project.tutorial`
+GROUP BY ALL
