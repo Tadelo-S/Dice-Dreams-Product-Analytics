5e777338b2748417484a0cac03afe233d17eda78
Tadelo-S <tadelo658@gmail.com>
2026-03-28 18:33:00 +0300

    Delete SQL/01_Onboarding/01_step_conversion.sql

--- SQL/01_Onboarding/01_step_conversion.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    step_index,
-    step_name,
-    COUNT(DISTINCT user_id) AS total_users_at_step
-FROM `ppltx-ba-course.final_project.tutorial`
-WHERE step_name is not null
-GROUP BY 1, 2
-ORDER BY 1 ASC;
